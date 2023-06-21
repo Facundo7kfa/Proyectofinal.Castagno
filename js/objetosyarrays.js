b94425a5f5d1de7190845a5b2c7786ac53d76493
Facundo7kfa <facundo.castagno77@outlook.com>
2023-06-20 23:53:11 -0300

    agregando objetos todavia no esta listo

---
+++ js/objetosyarrays.js
@@ -0,0 +1,57 @@
+const jupiter = {
+    nombre: "Jupiter",
+  
+    bitcoin: 3,
+    precio: 5000000
+  };
+
+  const marte = {
+    nombre: "Marte",
+
+    bitcoin: 4,
+    precio: 8000000
+  };
+
+  const urano = {
+    nombre: "Urano",
+
+    bitcoin: 3,
+    precio: 8000000
+  };
+
+  const ciudadEspacial = {
+    nombre: "Ciudad Espacial kfa.77",
+
+    bitcoin: 3,
+    precio: 5000000
+  };
+
+  const venus = {
+    nombre: "Venus",
+
+    mensajeMilitar: "Soy personal militar"
+  };
+
+  const mercurio = {
+    nombre: "Mercurio",
+
+    bitcoin: 3,
+    precio: 5000000
+  };
+
+  const saturno = {
+    nombre: "Saturno",
+    bitcoin: 3,
+    precio: 5000000
+  };
+
+  const neptuno = {
+    nombre: "Neptuno",
+    bitcoin: 3,
+    precio: 5000000
+  };
+
+  //  array de tarjetas
+  const tarjetas = [jupiter, marte, urano, ciudadEspacial, venus, mercurio, saturno, neptuno];
+
+ 
