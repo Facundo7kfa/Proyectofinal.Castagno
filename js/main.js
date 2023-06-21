360f96b7b985fa5466576df48998bd4338a0c128
Facundo7kfa <facundo.castagno77@outlook.com>
2023-06-21 01:11:33 -0300

    .

--- js/main.js
+++ js/main.js
@@ -1,4 +1,43 @@
 
+// Solicitar nombre
+let nombre = prompt("Ingresa tu nombre:");
+
+// Solicitar correo electrónico y validar si se ingresó un "@" utilizando un bucle while
+let correo = '';
+while (!correo.includes('@')) {
+  correo = prompt("Ingresa tu correo electrónico (debe contener '@'):");
+}
+
+// Solicitar contraseña
+let contraseña = prompt("Ingresa tu contraseña:");
+
+// Obtener la fecha actual
+const fecha = new Date();
+
+// Crear un objeto con los datos ingresados y la fecha
+const datos = {
+  nombre,
+  correo,
+  contraseña,
+  fecha
+};
+
+// Convertir el objeto a cadena de texto JSON
+const datosJSON = JSON.stringify(datos);
+
+// Guardar los datos en el SessionStorage
+localStorage.setItem("datos", datosJSON);
+
+// Mostrar una alerta de éxito
+alert("Los datos se han guardado exitosamente... bienvenido .");
+
+
+
+
+
+
+
+
 
  function solicitarCredenciales() {
     // Mostrar SweetAlert prompt para solicitar correo electrónico y contraseña
--- js/objetosyarrays.js
+++ js/objetosyarrays.js
@@ -1,40 +1,34 @@
 const jupiter = {
     nombre: "Jupiter",
-  
     bitcoin: 3,
     precio: 5000000
   };
 
   const marte = {
     nombre: "Marte",
-
     bitcoin: 4,
     precio: 8000000
   };
 
   const urano = {
     nombre: "Urano",
-
     bitcoin: 3,
     precio: 8000000
   };
 
   const ciudadEspacial = {
     nombre: "Ciudad Espacial kfa.77",
-
     bitcoin: 3,
     precio: 5000000
   };
 
   const venus = {
     nombre: "Venus",
-
     mensajeMilitar: "Soy personal militar"
   };
 
   const mercurio = {
     nombre: "Mercurio",
-
     bitcoin: 3,
     precio: 5000000
   };
@@ -54,4 +48,3 @@ const jupiter = {
   //  array de tarjetas
   const tarjetas = [jupiter, marte, urano, ciudadEspacial, venus, mercurio, saturno, neptuno];
 
- 
